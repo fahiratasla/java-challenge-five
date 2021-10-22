9c405c642f5a1ab0a62bc3c0cdf054aa86b0b70b
Fahira <fahiratasla13@gmail.com>
2021-10-22 12:06:10 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        Heroes hero = new Heroes();
+    
+        hero.setProfile("Fahira", 21, 163);
+        hero.profile();
+        
+    
+        // jalankan aktivitas disini...
+    
+        // contoh:
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+        hero.jalan();
+    
+        hero.lari();
+        hero.lari();
+        hero.lari();
+        hero.lari();
+    
+        hero.makan();
+        hero.makan();
+        hero.makan();
+        hero.makan();
+    
+        hero.minum();
+        hero.minum();
+        hero.minum();
+        hero.minum();
+    
+        hero.lompat();
+        hero.lompat();
+        hero.lompat();
+    
+        hero.duduk();
+        hero.duduk();
+    
+    
+        // challenge:
+        /*
+        Group 4: jalan 5x, lari 4x, makan 4x, minum 4x, lompat 3x, duduk 2x
+        */
+          
+        // tampilkan status terkini...
+        hero.profile();
+        
+      }
+    
+}
